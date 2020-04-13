1f3a38c602358dd6836a796fb71dad3176dc66a8
Prateek Agarwal <prateek.agarwal3@gmail.com>
2020-04-13 11:36:03 -0700

    parsing done, training_data array created

--- Persona Chat Dialogue/parsing_data.py
+++ Persona Chat Dialogue/parsing_data.py
@@ -1,12 +1,11 @@
-# path = 'data/personachat/train_self_original.txt'
-path = 'data/example_data.txt'
+path = 'data/personachat/train_self_original.txt'
+# path = 'data/example_data.txt'
 user_messages = []
 model_response_targets = []
 model_response_candidates = []
-model_personas = []
-cnt = 0
+model_persona = None
+train_data = []
 cands_are_replies = False
-i = 0
 
 def parse_persona(profile):
     modified_profile = []
@@ -15,16 +14,32 @@ def parse_persona(profile):
         modified_profile.append(p)
     return modified_profile
 
+def split_model_response_candidates(model_response_candidates):
+    model_response_candidates = model_response_candidates.split('|')
+    return model_response_candidates
+
 with open(path) as read:
     for line in read:
         line = line.strip().replace('\\n', '\n')
         if len(line) > 0:
-            cnt = cnt + 1
-            if cnt == 1 and line[0:2] == '1 ':
+            if line[0:2] == '1 ':
                 lines_have_ids = True
+                if len(user_messages) != 0:
+                    for i in range(len(user_messages)):
+                        training_example = {}
+                        training_example["user_message"] = user_messages[i]
+                        training_example["model_response_candidates"] = model_response_candidates[i]
+                        training_example["model_response_target"] = model_response_targets[i]
+                        training_example["model_persona"] = model_persona
+                        train_data.append(training_example)
+                    user_messages = []
+                    model_response_targets = []
+                    model_response_candidates = []
+                    model_persona = None
+                    cands_are_replies = False
             if '\t' in line and not cands_are_replies:
                 cands_are_replies = True
-                model_personas.append(parse_persona(model_response_targets))
+                model_persona = parse_persona(model_response_targets)
                 model_response_targets = []
             if lines_have_ids:
                 space_idx = line.find(' ')
@@ -34,30 +49,19 @@ with open(path) as read:
                     if len(sp) > 1 and sp[1] != '':
                         user_messages.append(sp[0])
                         model_response_targets.append(sp[1])
-                        model_response_candidates.append(sp[3])
-                    i +=1
+                        model_response_candidates.append(split_model_response_candidates(sp[3]))
                 else:
                     model_response_targets.append(line)
             else:
                 model_response_targets.append(line)
+    for i in range(len(user_messages)):
+        training_example = {}
+        # print(user_messages[i])
+        training_example["user_message"] = user_messages[i]
+        training_example["model_response_candidates"] = model_response_candidates[i]
+        training_example["model_response_target"] = model_response_targets[i]
+        training_example["model_persona"] = model_persona
+        train_data.append(training_example)
 
-# for i in range(len(model_response_candidates)):
-#     model_response_candidates[i] = model_response_candidates[i].split('|')
-
-print(len(model_personas))
-print('~~~~~~~~~~~~~')
-print(user_messages)
-print('~~~~~~~~~~~~~')
-print(len(model_response_targets))
-print('~~~~~~~~~~~~~')
-print(len(model_response_candidates))
-
-# train_data = []
-# training_example = {}
-# for i in range(len(user_messages)):
-#     training_example["user_message"] = user_messages[i]
-#     training_example["model_response_candidates"] = model_response_candidates[i]
-#     training_example["model_response_target"] = model_response_targets[i]
-#     training_example["model_persona"] = model_personas[0]
-#     train_data.append(training_example)
-# print('no of training examples are {}'.format(len(train_data)))
+# print('~~~~~~~~~~~~~')
+print('no of training examples are {}'.format(len(train_data)))
